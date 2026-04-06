35063c250d37902dfc37dac0d7977ea68b6d2469
Faraz-dev99 <arbazuddin242830@gmail.com>
2026-04-06 16:13:27 +0530

    first commit

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import app from "./app.js";
+import { syncCallLogsInternal } from "./jobs/syncCallLogs.js";
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+  setInterval(async () => {
+    try {
+     // console.log("🔄 Syncing call logs...");
+      await syncCallLogsInternal();
+    } catch (err) {
+      console.error("❌ Sync error:", err);
+    }
+  }, 10000);
+});
+
