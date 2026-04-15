b46375b08591dfbb10655cde05923bd033a31dfd
Faraz-dev99 <arbazuddin242830@gmail.com>
2026-04-15 12:10:18 +0530

    u

--- src/app.js
+++ src/app.js
@@ -52,7 +52,8 @@ app.use(
       "http://localhost:3000",
       "http://localhost:5678/",
       "https://app.estateai.in",
-      "https://estateai.in",     
+      "https://estateai.in",
+      "https://estateai-backuped.vercel.app/"     
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
