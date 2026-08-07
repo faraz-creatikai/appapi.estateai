31b0746038479ec75790136f4ce64a4deca13008
Faraz-dev99 <arbazuddin242830@gmail.com>
2026-08-07 12:23:16 +0530

    u

--- src/config/cors-origins.js
+++ src/config/cors-origins.js
@@ -2,8 +2,8 @@
 export const ALLOWED_ORIGINS = [
   "http://localhost:3000",
   "http://localhost:5678/",
-  "https://app.estateai.in",
   "https://estateai.in",
+  "https://app.estateai.in",
 ];
 
 export const clientUrl="https://app.estateai.in";
